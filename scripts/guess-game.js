1a379f62ae4df32f5ed3130d4b36d3e9eebc2587
Diniska 1488 <chikibryak72@gmail.com>
2025-12-24 17:46:57 +0100

    Js ferdig

---
+++ scripts/guess-game.js
@@ -0,0 +1,26 @@
+document.addEventListener('DOMContentLoaded', function() {
+    const checkButton = document.getElementById('check-button');
+    const restartButton = document.getElementById('restart-button');
+    const resultDisplay = document.querySelector('.result');
+    let NumberToGuess = Math.floor(Math.random() * 10) + 1;
+    let userGuess = null;
+
+    document.getElementById('guess-input').addEventListener('input', function(event) {
+        userGuess = parseInt(event.target.value, 10);
+    });
+
+    checkButton.addEventListener('click', function() {
+        if (userGuess === NumberToGuess) {
+            resultDisplay.textContent = "Correct!";
+        } else {
+            resultDisplay.textContent = "Wrong!";
+        }
+    });
+
+    restartButton.addEventListener('click', function() {
+        NumberToGuess = Math.floor(Math.random() * 10) + 1;
+        resultDisplay.textContent = "";
+        document.getElementById('guess-input').value = "";
+        userGuess = null;
+    });
+});
---
+++ scripts/pass-gen.js
@@ -0,0 +1,16 @@
+const symbols = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789!@#$%";
+
+function generatePassword(length) {
+    let password = "";
+    for (let i = 0; i < length; i++) {
+        password += symbols.charAt(Math.floor(Math.random() * symbols.length));
+    }
+    return password;
+}
+
+document.addEventListener('DOMContentLoaded', function() {
+    document.getElementById("generate-password").addEventListener("click", function() {
+        let passwordOutput = generatePassword(8);
+        document.getElementById("password-output").textContent = passwordOutput;
+    });
+});
